35598d5cdd753263105dfe482fa30312b9932632
Andrey Ermolchik <frutizzzzz@gmail.com>
2021-08-10 11:51:02 +0300

    lesson4 Payment

---
+++ lession1/src/main/java/school/lesson4_3/Payment2.java
@@ -0,0 +1,29 @@
+package school.lesson4_3;
+
+import java.io.IOException;
+import school.lesson4_3.Payment;
+
+public class Payment2 {
+    public static void main(String [] args) throws IOException{
+        try {
+            Payment pay1 = new Payment("Первая покупка");
+            pay1.setPayment();
+
+            Payment pay2 = new Payment("Вторая покупка");
+            pay2.setPayment();
+
+            pay1.printCheque();
+            pay2.printCheque();
+        }
+        catch(NumberFormatException e){
+            System.out.println("Неверный формат");
+              }
+        catch(NullPointerException e){
+            System.out.println("Массив не создан");
+        }
+
+        }
+    }
+
+
+
